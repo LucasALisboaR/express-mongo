f79449b9faacf18babe993313dc7583deff841d0
LucasLisboa <lucas.lisboa@enacom.com.br>
2024-10-07 19:46:58 -0300

    feat: ajust controller and app js, create rotas

--- src/controlers/livroController.js
+++ src/controlers/livroController.js
@@ -3,9 +3,41 @@ import livro from "../models/Livro.js";
 class LivroController {
 
     static async listarLivros(req, res) {
-        const listaLivros = await livro.find({});
-        res.status(200).json(listaLivros);
-    }
+        try {
+            const listaLivros = await livro.find({});
+            res.status(200).json(listaLivros);
+        } catch (erro) {
+            res.status(500).json({
+                message: `${erro.message} - falha ao buscar livros`
+            })
+        }
+    };
+
+    static async listarLivro(req, res) {
+        try {
+            const id = req.params.id;
+            const livroEncontrado = await livro.findById(id);
+            res.status(200).json(livroEncontrado);
+        } catch (erro) {
+            res.status(500).json({
+                message: `${erro.message} - falha ao buscar livro`
+            })
+        }
+    };
+
+    static async atualizarLivro(req, res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndUpdate(id, req.body);
+            res.status(200).json({
+                message: 'Alterado com sucesso!'
+            });
+        } catch (erro) {
+            res.status(500).json({
+                message: `${erro.message} - falha ao atualizar livro`
+            })
+        }
+    };
 
     static async cadastrarLivro(req, res) {
         try {
@@ -19,7 +51,21 @@ class LivroController {
                 message: `${erro.message} - falha ao cadastrar livro`
             })
         }
-    }
+    };
+
+    static async deletarLivro(req, res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndDelete(id);
+            res.status(200).json({
+                message: 'Deletado com sucesso!'
+            });
+        } catch (erro) {
+            res.status(500).json({
+                message: `${erro.message} - falha ao excluir livro`
+            })
+        }
+    };
 
 };
 
