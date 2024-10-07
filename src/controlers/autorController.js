47e343ed8c38b4bc96bb49ec4100a4559e3da930
LucasLisboa <lucas.lisboa@enacom.com.br>
2024-10-07 20:03:51 -0300

    feat: create autores

---
+++ src/controlers/autorController.js
@@ -0,0 +1,72 @@
+import autor from "../models/Autor";
+
+class AutorController {
+
+    static async listarAutores(req, res) {
+        try {
+            const listaAutores = await autor.find({});
+            res.status(200).json(listaAutores);
+        } catch (erro) {
+            res.status(500).json({
+                message: `${erro.message} - falha ao buscar autores`
+            })
+        }
+    };
+
+    static async listarAutor(req, res) {
+        try {
+            const id = req.params.id;
+            const autorEncontrado = await autor.findById(id);
+            res.status(200).json(autorEncontrado);
+        } catch (erro) {
+            res.status(500).json({
+                message: `${erro.message} - falha ao buscar autor`
+            })
+        }
+    };
+
+    static async atualizarAutor(req, res) {
+        try {
+            const id = req.params.id;
+            await autor.findByIdAndUpdate(id, req.body);
+            res.status(200).json({
+                message: 'Alterado com sucesso!'
+            });
+        } catch (erro) {
+            res.status(500).json({
+                message: `${erro.message} - falha ao atualizar autor`
+            })
+        }
+    };
+
+    static async cadastrarAutor(req, res) {
+        try {
+            const novoAutor = await autor.create(req.body);
+            res.status(201).json({
+                message: 'Criado com sucesso!',
+                livro: novoAutor
+            });
+        } catch (erro) {
+            res.status(500).json({
+                message: `${erro.message} - falha ao cadastrar autor`
+            })
+        }
+    };
+
+    static async deletarAutor(req, res) {
+        try {
+            const id = req.params.id;
+            await autor.findByIdAndDelete(id);
+            res.status(200).json({
+                message: 'Deletado com sucesso!'
+            });
+        } catch (erro) {
+            res.status(500).json({
+                message: `${erro.message} - falha ao excluir autor`
+            })
+        }
+    };
+
+};
+
+export default AutorController;
